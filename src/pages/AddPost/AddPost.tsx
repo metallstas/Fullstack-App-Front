658d2060436ee3a5e4a6d0c1666eb256c9e6c738
Стас Хмылко <metallstas@mail.ru>
2025-05-19 19:21:46 +0300

    add text editor

--- src/pages/AddPost/AddPost.tsx
+++ src/pages/AddPost/AddPost.tsx
@@ -1,32 +1,40 @@
+import { useState } from 'react'
+
 import { Button } from '@/UI/Button/Button'
 import { Input } from '@/UI/Input/Input'
 
 import './add-post.scss'
-import { TextPanel } from '@/components/TextPanel/TextPanel'
+import { TextEditor } from '@/components/TextEditor/TextEditor'
 
 const AddPost = () => {
-    return (
-        <section className="add-post">
-            <div className="add-post__container">
-                <Button
-                    text="Загрузить превью"
-                    additionalClass="add-post__btn"
-                />
-                <Input
-                    customClass="add-post__title"
-                    type={'text'}
-                    placeholder="Заголовок статьи"
-                />
-                <Input
-                    customClass="add-post__tags"
-                    type={'text'}
-                    placeholder="#Тэги"
-                />
-                <TextPanel />
-                <input placeholder="Текст Поста" />
-            </div>
-        </section>
-    )
+	const [text, setText] = useState<string>('')
+	return (
+		<section className="add-post">
+			<div className="add-post__container">
+				<Button
+					text="Загрузить превью"
+					additionalClass="add-post__btn"
+				/>
+				<Input
+					customClass="add-post__title"
+					type={'text'}
+					placeholder="Заголовок статьи"
+				/>
+				<Input
+					customClass="add-post__tags"
+					type={'text'}
+					placeholder="#Тэги"
+				/>
+				<TextEditor />
+				<textarea
+					onChange={(e) => setText(e.target.value)}
+					value={text}
+					className="add-post__text"
+					placeholder="Текст Поста"
+				/>
+			</div>
+		</section>
+	)
 }
 
 export const Component = AddPost
