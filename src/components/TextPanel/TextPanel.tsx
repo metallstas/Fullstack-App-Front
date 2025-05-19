073d9bb28f29d2de4fe07f45ef8796f67d5ff491
Stas Khmylko <metallstas@mail.ru>
2025-05-19 17:56:16 +0300

    text panel

--- src/components/TextPanel/TextPanel.tsx
+++ src/components/TextPanel/TextPanel.tsx
@@ -1,3 +1,12 @@
+import FormatItalicIcon from '@mui/icons-material/FormatItalic'
+import './text-panel.scss'
+
 export const TextPanel = () => {
-    return <div className="text-panel">Text Panel</div>
+    return (
+        <div className="text-panel">
+            <div>
+                <FormatItalicIcon className="italic" />
+            </div>
+        </div>
+    )
 }
