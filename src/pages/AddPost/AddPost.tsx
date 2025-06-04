102abe843f42828b8a9e228c5735e4baf0ea4093
Стас Хмылко <metallstas@mail.ru>
2025-06-04 22:12:01 +0300

    comment

--- src/pages/AddPost/AddPost.tsx
+++ src/pages/AddPost/AddPost.tsx
@@ -3,31 +3,34 @@ import { Input } from '@/UI/Input/Input'
 import { TextEditor } from '@/modules/TextEditor/TextEditor'
 
 import './add-post.scss'
-
+//comment
 const AddPost = () => {
-    return (
-        <section className="add-post">
-            <div className="add-post__container">
-                <Button
-                    text="Загрузить превью"
-                    additionalClass="add-post__btn"
-                />
-                <Input
-                    value=""
-                    customClass="add-post__title"
-                    type={'text'}
-                    placeholder="Заголовок статьи"
-                />
-                <Input
-                    value=""
-                    customClass="add-post__tags"
-                    type={'text'}
-                    placeholder="#Тэги"
-                />
-                <TextEditor />
-            </div>
-        </section>
-    )
+	return (
+		<section className="add-post">
+			<div className="add-post__container">
+				<Button
+					onClick={() => {}}
+					text="Загрузить превью"
+					additionalClass="add-post__btn"
+				/>
+				<Input
+					onChange={() => {}}
+					value=""
+					customClass="add-post__title"
+					type={'text'}
+					placeholder="Заголовок статьи"
+				/>
+				<Input
+					onChange={() => {}}
+					value=""
+					customClass="add-post__tags"
+					type={'text'}
+					placeholder="#Тэги"
+				/>
+				<TextEditor />
+			</div>
+		</section>
+	)
 }
 
 export const Component = AddPost
